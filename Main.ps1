a0b97073d3011d97d711af5b73289e1be13e6bfa
Christian <Christian@escolano.ch>
2021-04-01 10:26:39 +0200

    Init Commit SQL Connector

---
+++ Main.ps1
@@ -0,0 +1,10 @@
+﻿# 31.03.2021 - CE
+
+$sqlServerIP = "192.168.10.246"
+$sqlusername = "username"
+$sqlpassword = "password"
+
+Import-Module "C:\Users\Christian\Desktop\Sync\HF\DB2\DB\Powershell\SQLConnctors.psm1" -Verbose
+
+GetProstgresSQLData -PostgreIP $sqlServerIP -Username $sqlusername -Password $sqlpassword -Database "nv_alarm" -SqlQuery "SELECT * FROM public.alarm_data ORDER BY id ASC LIMIT 100"
+
---
+++ SQLConnctors.psm1
@@ -0,0 +1,46 @@
+﻿# 30.03.2021 - CE
+# 31.03.2021 - CE
+
+function ConnectODBCData{
+
+    param(  
+        [string]$ConectionString=$(throw "ConectionString is required."),
+        [string]$SqlQuery=$(throw "SQL Comannd is required.")
+    )
+    echo $ConectionString
+
+    $ODBCconnector=New-Object System.Data.Odbc.OdbcConnection
+    $ODBCconnector.ConnectionString= $ConectionString
+    $ODBCconnector.open()
+
+    $command=new-object System.Data.Odbc.OdbcCommand($SqlQuery,$ODBCconnector)
+    $command.CommandTimeout=15
+
+    $ds=New-Object system.Data.DataSet
+    $da=New-Object system.Data.odbc.odbcDataAdapter($command)
+    [void]$da.fill($ds)
+    $ds.Tables[0]
+
+    $ODBCconnector.close()
+}
+
+function GetProstgresSQLData{
+
+    param(  
+        $PostgreIP=$(throw "IP or FQDN is required."), 
+        $PostgrePort="5432", 
+        $Username=$(throw "Username is required."), 
+        $Password=$(throw "Password is required."), 
+        $Database=$(throw "Database is required."),
+        $SqlQuery = $(throw "SQL Comannd is required.")
+        )
+
+    $PostgreSQLConnection =  "Driver={PostgreSQL Unicode(x64)};"
+    $PostgreSQLConnection += "Server=" + $PostgreIP + ";"
+    $PostgreSQLConnection += "Port=" + $PostgrePort + ";"
+    $PostgreSQLConnection += "Database=" + $Database + ";"
+    $PostgreSQLConnection += "Uid=" + $Username + ";"
+    $PostgreSQLConnection += "Pwd=" + $Password + ";"
+
+    ConnectODBCData -ConectionString $PostgreSQLConnection -SqlQuery $SqlQuery
+}
